493078414538f5d1e391e637dce65aa792c3f14c
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-03-31 10:36:17 +0530

    bank charge page layout done

--- bank-project/src/app/bank-charge/bank-charge.component.ts
+++ bank-project/src/app/bank-charge/bank-charge.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-bank-charge',
   templateUrl: './bank-charge.component.html',
-  styleUrls: ['./bank-charge.component.css']
+  styleUrls: ['./bank-charge.component.css'],
 })
 export class BankChargeComponent {
-
+  sendData(data: NgForm) {
+    console.log(data);
+  }
 }
